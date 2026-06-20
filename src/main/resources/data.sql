922a54f0926eb7a6eda2418ff06f417ef5db542c
Nepovezlo <sasasuharevskij98@gmail.com>
2026-06-20 10:57:22 +0300

    Fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,15 @@
-DELETE FROM film_genres;
-DELETE FROM films;
-DELETE FROM likes;
-DELETE FROM friends;
-DELETE FROM users;
-DELETE FROM genre;
-DELETE FROM mpa;
-DELETE FROM friendship_status;
+MERGE INTO genre (id, name) VALUES
+    (1, 'Комедия'),
+    (2, 'Драма'),
+    (3, 'Мультфильм'),
+    (4, 'Триллер'),
+    (5, 'Документальный'),
+    (6, 'Боевик');
 
--- Добавляем рейтинги MPA (американская система)
-INSERT INTO mpa (mpa_name) VALUES ('G');
-INSERT INTO mpa (mpa_name) VALUES ('PG');
-INSERT INTO mpa (mpa_name) VALUES ('PG-13');
-INSERT INTO mpa (mpa_name) VALUES ('R');
-INSERT INTO mpa (mpa_name) VALUES ('NC-17');
-
--- Добавляем жанры
-INSERT INTO genre (genre_name) VALUES ('Комедия');
-INSERT INTO genre (genre_name) VALUES ('Драма');
-INSERT INTO genre (genre_name) VALUES ('Мультфильм');
-INSERT INTO genre (genre_name) VALUES ('Триллер');
-INSERT INTO genre (genre_name) VALUES ('Документальный');
-INSERT INTO genre (genre_name) VALUES ('Боевик');
-
--- Добавляем статусы дружбы
-INSERT INTO friendship_status (status_check) VALUES ('UNCONFIRMED'), ('CONFIRMED');
+-- Инициализация MPA рейтингов
+MERGE INTO mpa (id, name) VALUES
+    (1, 'G'),
+    (2, 'PG'),
+    (3, 'PG-13'),
+    (4, 'R'),
+    (5, 'NC-17');
